c46618b6341330165507e8332b52cfe92e63b92e
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-04-08 16:40:55 +0300

    fixing bugs

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const methodOverride = require( "method-override" );
 const bodyParser = require( "body-parser" );
 const path = require( "path" );
 const morgan = require( "morgan" );
+const ExpressError = require( "./utils/ExpressError" );
 const app = express();
 const port = process.env.PORT || 3000
 
@@ -36,41 +37,65 @@ app.use( methodOverride( "_method" ) );
 
 app.get( "/", async ( req, res ) =>
 {
-	const data = await Course.find( {} )
-		.populate( { path: "lectures" } );
-	console.log( "hit" );
-	res.send( data );
+	try
+	{
+		const data = await Course.find( {} )
+			.populate( { path: "lectures" } );
+		console.log( "hit" );
+		res.send( data );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.get( "/admin", async ( req, res ) =>
 {
-	const lecture = await Lecture.find( {} );
-	const data = await Course.find( {} )
-		.populate( { path: "lectures" } );
-	console.log( data, lecture );
-	console.log( "hit" );
-	res.render( "index", { data } );
+	try
+	{
+		const lecture = await Lecture.find( {} );
+		const data = await Course.find( {} )
+			.populate( { path: "lectures" } );
+		console.log( data, lecture );
+		console.log( "hit" );
+		res.render( "index", { data } );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.get( "/admin/course/:courseId/new-lecture", async ( req, res ) =>
 {
-	const course = await Course.findById( req.params.courseId );
-	console.log( course );
-	res.render( "new-lecture", { course } );
+	try
+	{
+		const course = await Course.findById( req.params.courseId );
+		console.log( course );
+		res.render( "new-lecture", { course } );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.get( "/admin/course/:courseId/:lectureId", async ( req, res ) =>
 {
-	const course = await Course.findOne( { _id: req.params.courseId } )
-		.populate( { path: "lectures" } );
-	const lectures = course.lectures.map( lecture => lecture );
-	const lecture = lectures.find( lecture =>
+	try
 	{
-		return lecture._id == req.params.lectureId;
-	} );
-	console.log( lecture );
-	console.log( "hit" );
-	res.render( "lecture", { lecture, course } );
+		const course = await Course.findOne( { _id: req.params.courseId } )
+			.populate( { path: "lectures" } );
+		const lectures = course.lectures.map( lecture => lecture );
+		const lecture = lectures.find( lecture =>
+		{
+			return lecture._id == req.params.lectureId;
+		} );
+		console.log( lecture );
+		console.log( "hit" );
+		res.render( "lecture", { lecture, course } );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.get( "/admin/new", ( req, res ) =>
@@ -81,46 +106,86 @@ app.get( "/admin/new", ( req, res ) =>
 
 app.post( "/admin/new", async ( req, res ) =>
 {
-	const course = new Course( req.body );
-	await course.save();
-	console.log( "hit" );
-	console.log( "Done", course );
-	res.redirect( "/admin" );
+	try
+	{
+		const course = new Course( req.body );
+		await course.save();
+		console.log( "hit" );
+		console.log( "Done", course );
+		res.redirect( "/admin" );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.put( "/admin/course/:courseId/:lectureId", async ( req, res ) =>
 {
-	const lecture = await Lecture.findByIdAndUpdate( req.params.lectureId, req.body );
-	console.log( "hit" );
-	console.log( lecture );
-	res.redirect( "/admin" );
+	try
+	{
+		const lecture = await Lecture.findByIdAndUpdate( req.params.lectureId, req.body );
+		console.log( "hit" );
+		console.log( lecture );
+		res.redirect( "/admin" );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.delete( "/admin/course/:courseId", async ( req, res ) =>
 {
-	await Course.findByIdAndDelete( req.params.courseId );
-	console.log( "hit" );
-	console.log( "done" );
-	res.redirect( "/admin" );
+	try
+	{
+		await Course.findByIdAndDelete( req.params.courseId );
+		console.log( "hit" );
+		console.log( "done" );
+		res.redirect( "/admin" );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 
 app.post( "/admin/course/:courseId/new-lecture", async ( req, res ) =>
 {
-	const course = await Course.findById( req.params.courseId );
-	const lecture = new Lecture( req.body );
-	course.lectures.push( lecture );
-	await lecture.save();
-	await course.save();
-	console.log( course, lecture );
-	res.redirect( "/admin" );
+	try
+	{
+		const course = await Course.findById( req.params.courseId );
+		const lecture = new Lecture( req.body );
+		course.lectures.push( lecture );
+		await lecture.save();
+		await course.save();
+		console.log( course, lecture );
+		res.redirect( "/admin" );
+	} catch ( error )
+	{
+		console.log( error );
+	}
 } );
 
 app.delete( "/admin/course/:courseId/:lectureId", async ( req, res ) =>
 {
-	await Lecture.findByIdAndDelete( req.params.lectureId );
-	console.log( "done" );
-	res.redirect( "/admin" );
+	try
+	{
+		await Lecture.findByIdAndDelete( req.params.lectureId );
+		console.log( "done" );
+		res.redirect( "/admin" );
+	} catch ( error )
+	{
+		console.log( error );
+	}
+} );
+app.all( "*", ( req, res, next ) =>
+{
+	next( new ExpressError( "Page Not Found", 404 ) );
+} );
+app.use( ( err, req, res, next ) =>
+{
+	const { statusCode = 500 } = err;
+	if ( !err.message ) err.message = "Oh No, Something Went Wrong!";
+	res.status( statusCode ).send( err );
 } );
 app.listen( port, () =>
 {
