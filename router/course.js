82b41af1e3a26cc819e14894edb6e1006cdfc43a
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-04-09 01:27:54 +0300

    fix huge bug was causing me sleepness

--- router/course.js
+++ router/course.js
@@ -5,7 +5,7 @@ const catchAsync = require( '../utils/catchAsync' );
 
 router.route( '/' ).get( catchAsync( courseController.index ) );
 router.route( '/admin' ).get( catchAsync( courseController.renderAdmin ) );
-router.route( '/admin/course/:courseId/new-lecture' ).get( catchAsync( catchAsync( courseController.renderNewLecture ) ) );
+router.route( '/admin/course/:courseId/new-lecture' ).get( catchAsync( courseController.renderNewLecture ) );
 router.route( '/admin/course/:courseId/:lectureId' ).get( catchAsync( courseController.renderLecture ) );
 router.route( '/admin/new' ).get( courseController.renderNewCourse ).post( catchAsync( courseController.createCourse ) );
 router.route( '/admin/course/:courseId/:lectureId' ).put( catchAsync( courseController.updateLecture ) );
