0734502168ed6cbe109cf5afa5e51f372cc78a95
Ismail Salah <111834969+IsmailJunior@users.noreply.github.com>
2023-04-08 16:49:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,10 +28,11 @@ async function main ()
 app.engine( "ejs", ejsMate );
 app.set( "view engine", "ejs" );
 app.set( "views", path.join( __dirname, "views" ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use(express.urlencoded({ extended: true }));
 app.use( express.static( path.join( __dirname, "public" ) ) );
 app.use( cors() );
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( morgan( "dev" ) );
 app.use( methodOverride( "_method" ) );
 
